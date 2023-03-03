0d770797e07724305e47a37fd829f8db9cfb536c
Leonardo Pacheco <lpachecoc@unal.edu.co>
2023-03-03 00:01:19 -0500

    Jenkins installed and running from docker-compose

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script will download and start Apache, and then create asymlink between your synced files directory and the location whereApache will look for the content
-apt-get update
-apt-get install -y apache2
-if ! [ -L /var/www ]; then
-    rm -rf /var/www
-    ln -fs /vagrant /var/www
-fi
